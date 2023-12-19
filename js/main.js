6ee597fdea378665bd79aa1d42878a49dc560315
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2023-12-19 11:08:14 +0100

    Added variables

--- js/main.js
+++ js/main.js
@@ -2,50 +2,65 @@
 
 const michis = document.querySelector('.js-list');
 
+const kittenImageOne = 'https://dev.adalab.es/gato-siames.webp';
+const kittenNameOne = 'Anastacio';
+const kittenDescOne = `Porte elegante, su patrón de color tan característico y sus ojos
+de un azul intenso, pero su historia se remonta a Asía al menos
+hace 500 años, donde tuvo su origen muy posiblemente.`;
+const kittenRaceOne = 'Siamés';
+
 const kittenOne =` <li class="card">
 <article>
   <img
     class="card_img"
-    src="https://dev.adalab.es/gato-siames.webp"
+    src= ${kittenImageOne}
     alt="siames-cat"
   />
-  <h3 class="card_title">Anastacio</h3>
-  <h4 class="card_race">Siamés</h4>
+  <h3 class="card_title">${kittenNameOne.toUpperCase()}</h3>
+  <h4 class="card_race">${kittenRaceOne}</h4>
   <p class="card_description">
-    Porte elegante, su patrón de color tan característico y sus ojos
-    de un azul intenso, pero su historia se remonta a Asía al menos
-    hace 500 años, donde tuvo su origen muy posiblemente.
+  ${kittenDescOne}
   </p>
 </article>
 </li>`;
 
+const kittenImageTwo = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenNameTwo = 'Fiona';
+const kittenDescTwo = `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+hasta con pinta de alienígena han llegado a definir a esta raza
+gatuna que se caracteriza por la «ausencia» de pelo.`;
+const kittenRaceTwo = 'Sphynx';
+
 const kittenTwo = `<li class="card">
 <img
   class="card_img"
-  src="https://dev.adalab.es/sphynx-gato.webp"
+  src=${kittenImageTwo}
   alt="sphynx-cat"
 />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">Sphynx</h4>
+<h3 class="card_title">${kittenNameTwo.toUpperCase()}</h3>
+<h4 class="card_race">${kittenRaceTwo}</h4>
 <p class="card_description">
-  Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-  hasta con pinta de alienígena han llegado a definir a esta raza
-  gatuna que se caracteriza por la «ausencia» de pelo.
+${kittenDescTwo}
 </p>
 </li>`;
+
+const kittenImageThree = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenNameThree = 'Cielo';
+const kittenDescThree = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+bella mirada se ha convertido en una de sus señas de identidad.
+Sus ojos son grandes y las orejas resultan largas y en punta.`;
+const kittenRaceThree = 'Maine Coon';
 
 const kittenThree = `<li class="card">
 <img
   class="card_img"
-  src="https://dev.adalab.es/maine-coon-cat.webp"
-  alt="maine-coon-cat"
+  src=${kittenImageThree}
+  alt="sphynx-cat"
 />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">Maine Coon</h4>
+<h3 class="card_title">${kittenNameThree.toUpperCase()}</h3>
+<h4 class="card_race">${kittenRaceThree}</h4>
 <p class="card_description">
-  Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-  bella mirada se ha convertido en una de sus señas de identidad.
-  Sus ojos son grandes y las orejas resultan largas y en punta.
+${kittenDescThree}
 </p>
 </li>`;
 

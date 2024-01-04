b8c95e82f237e063f634416213bb89ea3abb853d
Mbueno992 <mbueno_92@hotmail.com>
2024-01-04 11:21:14 +0100

    solution renderKitten

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
 'use strict';
 
+//constantes
 const michis = document.querySelector('.js-list');
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const formSection = document.querySelector('.js-new-form');
+const inputDesc = document.querySelector('.js-input-desc');
+const inputPhoto = document.querySelector('.js-input-photo');
+const inputName = document.querySelector('.js-input-name');
+const inputRace = document.querySelector('.js-input-race');
+const labelMessageError = document.querySelector('.js-label-error');
+const buttonSearch = document.querySelector('.js-button-search');
+const buttonCancelForm = document.querySelector('.js-btn-cancel');
+const kittenButton = document.querySelector('.js-AdaKitten');
+const addKitten = document.querySelector('.js-btn-add');
+const raceFilter = document.querySelector('.js-inputRace');
 
+// 01. Mostrar gatitos desde JS
 const kittenImageOne = 'https://dev.adalab.es/gato-siames.webp';
 const kittenNameOne = 'Anastacio';
 const kittenDescOne = `Porte elegante, su patrón de color tan característico y sus ojos
 de un azul intenso, pero su historia se remonta a Asía al menos
 hace 500 años, donde tuvo su origen muy posiblemente.`;
 const kittenRaceOne = 'Siamés';
+
+const kittenImageTwo = 'https://dev.adalab.es/sphynx-gato.webp';
+const kittenNameTwo = 'Fiona';
+const kittenDescTwo = `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+hasta con pinta de alienígena han llegado a definir a esta raza
+gatuna que se caracteriza por la «ausencia» de pelo.`;
+const kittenRaceTwo = 'Sphynx';
+
+const kittenImageThree = 'https://dev.adalab.es/maine-coon-cat.webp';
+const kittenNameThree = 'Cielo';
+const kittenDescThree = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+bella mirada se ha convertido en una de sus señas de identidad.
+Sus ojos son grandes y las orejas resultan largas y en punta.`;
+const kittenRaceThree = 'Maine Coon';
 
 const kittenOne = ` <li class="card">
 <article>
@@ -24,13 +52,6 @@ const kittenOne = ` <li class="card">
 </article>
 </li>`;
 
-const kittenImageTwo = 'https://dev.adalab.es/sphynx-gato.webp';
-const kittenNameTwo = 'Fiona';
-const kittenDescTwo = `Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-hasta con pinta de alienígena han llegado a definir a esta raza
-gatuna que se caracteriza por la «ausencia» de pelo.`;
-const kittenRaceTwo = 'Sphynx';
-
 const kittenTwo = `<li class="card">
 <img
   class="card_img"
@@ -43,13 +64,6 @@ const kittenTwo = `<li class="card">
 ${kittenDescTwo}
 </p>
 </li>`;
-
-const kittenImageThree = 'https://dev.adalab.es/maine-coon-cat.webp';
-const kittenNameThree = 'Cielo';
-const kittenDescThree = `Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-bella mirada se ha convertido en una de sus señas de identidad.
-Sus ojos son grandes y las orejas resultan largas y en punta.`;
-const kittenRaceThree = 'Maine Coon';
 
 const kittenThree = `<li class="card">
 <img
@@ -64,23 +78,14 @@ ${kittenDescThree}
 </p>
 </li>`;
 
+const kittenData_1 = {
+  image: 'https://dev.adalab.es/gato-siames.webp',
+  name: 'Anastacio',
+  desc: ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: 'Siamés',
+};
+
 michis.innerHTML = kittenOne + kittenTwo + kittenThree;
-
-//ejercicio condicionales
-const input_search_desc = document.querySelector('.js_in_search_desc');
-const descrSearchText = input_search_desc.value;
-
-// if (kittenDescOne.includes(descrSearchText)) {
-//   console.log(`${kittenNameOne} contiene la palabra ${descrSearchText}`);
-// }
-
-// if (kittenDescTwo.includes(descrSearchText)) {
-//   console.log(`${kittenNameTwo} contiene la palabra ${descrSearchText}`);
-// }
-
-// if (kittenDescThree.includes(descrSearchText)) {
-//   console.log(`${kittenNameThree} contiene la palabra ${descrSearchText}`);
-// }
 
 /*let html = '';
 
@@ -93,111 +98,89 @@ const descrSearchText = input_search_desc.value;
 
 //Ejercicio 4. Eventos
 
-const formSection = document.querySelector('.js-new-form');
-const kittenButton = document.querySelector('.js-AdaKitten');
-formSection.classList.remove('collapsed')
-
-const addKitten = document.querySelector('.js-btn-add');
-const inputDesc = document.querySelector('.js-input-desc');
-const inputPhoto = document.querySelector('.js-input-photo');
-const inputName = document.querySelector('.js-input-name');
-const inputRace = document.querySelector('.js-input-race');
-const labelMessageError = document.querySelector('.js-label-error');
-const buttonCancel = document.querySelector('.js-cancel')
-
-
-
 //Al pulsar el botón cancelar se cierra el formulario, faltaría resetear los campos.
-buttonCancel.addEventListener('click', (cancel)=>{
-  cancel.preventDefault();
+buttonCancelForm.addEventListener('click', (event) => {
+  event.preventDefault();
   formSection.classList.add('collapsed');
-  
 });
 
-//Ejercicio FORMULARIO: MOstrar/ocultar
 
+//Ejercicio Funciones I - Formulario: Mostrar/ocultar
+
+function hideNewCatForm() {
+  formSection.classList.add('collapsed');
+}
 function showNewCatForm() {
   formSection.classList.remove('collapsed');
 }
-function hideNewCatForm() {
-  formSection.classList.add('collapsed');
+
+function handleClickNewCatForm() {
+  if (formSection.classList.contains('collapsed')) {
+    showNewCatForm();
+  } else {
+    hideNewCatForm();
+  }
 }
 
 kittenButton.addEventListener('click', handleClickNewCatForm);
 
-function handleClickNewCatForm(event) {
-  event.preventDefault();
-  if (formSection.classList.contains('collapsed')) {
-    formSection.classList.remove('collapsed');
-  } else {
-    formSection.classList.add('collapsed');
-  }
-}
-
 //Ejercicio Adicionar nuevo gatito
 
-
-
-
-function renderKitten() {
-  const valueDesc = inputDesc.value;
-  const valuePhoto = inputPhoto.value;
-  const valueName = inputName.value;
-  const valueRace = inputRace.value;
-  michis.innerHTML += (`<li class="card">
+function renderKitten(kittenData) {
+  const html = `<li class="card">
   <article>
     <img
       class="card_img"
-      src= "${valuePhoto}"
-      alt="siames-cat"
+      src="${kittenData.img}"
+      alt="imagen de gato"
     />
-    <h3 class="card_title">${valueName.toUpperCase()}</h3>
-    <h4 class="card_race">${valueRace}</h4>
+    <h3 class="card_title">${kittenData.name.toUpperCase()}</h3>
+    <h4 class="card_race">${kittenData.race}</h4>
     <p class="card_description">
-    ${valueDesc}
+    ${kittenData.desc}
     </p>
   </article>
-  </li>`);
+  </li>`;
+  michis.innerHTML += html;
 }
-// renderKitten(`${valuePhoto}, ${valueName}, ${valueRace}, ${valueDesc}`)
 
-
-function addNewKitten() {
-  const valueDesc = inputDesc.value;
-  const valuePhoto = inputPhoto.value;
-  const valueName = inputName.value;
-  const valueRace = inputRace.value;
-  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
+function addNewKitten(event) {
+  event.preventDefault();
+   const kittenData ={
+    image: inputPhoto.value,
+    name: inputName.value,
+    desc: inputDesc.value,
+    race: inputRace.value,
+  }
+  if (kittenData.desc === '' || kittenData.image === '' || kittenData.name === '') {
 
     labelMessageError.innerHTML ='¡Uy! parece que has olvidado algo';
 
   }else{ 
     labelMessageError.innerHTML = 'Gatito añadido correctamente';
-    renderKitten()
-    }
+    renderKitten(kittenData);
+   };
   }
 
 addKitten.addEventListener('click', addNewKitten);
 
-
 // ejercicio filtrar 
-
-const buttonSearch = document.querySelector('.js-button-search');
-
 const filterKitten = (event) => {
   event.preventDefault();
+  michis.innerHTML = '';
+  const descrSearchText = input_search_desc.value;
+  const race = raceFilter.value;
   if (kittenDescOne.includes(descrSearchText)) {
     michis.innerHTML += kittenOne;
   }
-  if (kittenDescTwo.includes(descrSearchText)) {
+  if (kittenRaceOne.includes(race)){
+    michis.innerHTML += kittenOne;
+  }
+  if (kittenDescTwo.includes(descrSearchText) || kittenRaceTwo.includes(race)) {
     michis.innerHTML += kittenTwo;
-    console.log(`${kittenNameTwo} contiene la palabra ${descrSearchText}`);
   }
-  if (kittenDescThree.includes(descrSearchText)) {
+  if (kittenDescThree.includes(descrSearchText) || kittenRaceThree.includes(race)) {
     michis.innerHTML += kittenThree;
-  }
-  else{
-    console.log('no hay gatitos con la palabra');
   }
 };
 
